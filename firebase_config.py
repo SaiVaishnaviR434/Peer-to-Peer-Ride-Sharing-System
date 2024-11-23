f07004bde150a33a68ef306194fd175b06adfd80
Sai Vaishnavi <152798109+SaiVaishnaviR434@users.noreply.github.com>
2024-11-23 17:49:20 +0530

    Add files via upload

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyCJxrzonTR_q1rvyClYbmMuhey3x7xtzys",
+  "authDomain": "peer2peer-ride-sharing-system.firebaseapp.com",
+  "databaseURL": "https://peer2peer-ride-sharing-system-default-rtdb.firebaseio.com",
+  "projectId": "peer2peer-ride-sharing-system",
+  "storageBucket": "peer2peer-ride-sharing-system.firebasestorage.app",
+  "messagingSenderId": "498115418248",
+  "appId": "1:498115418248:web:d34abdc65a38de25d0a73d",
+  "measurementId": "G-NBFRMHG2TE"
+}
